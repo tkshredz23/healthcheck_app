69ce385c9a5428b31cbe0d252e3bebcc7e0ef11a
Tikky Shiwala <tshiwala@constantcontact.com>
2017-11-07 12:29:06 -0500

    added model specs

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -9,7 +9,7 @@ describe Status, type: :model do
     before { create_list(:status, 3) }
     let!(:status) { create(:status, stat: 'down', message: 'i am down') }
 
-    it 'returns the right status' do
+    it 'returns the latest status' do
       expect(Status.current_status).to eq('down')
     end
   end
@@ -19,6 +19,32 @@ describe Status, type: :model do
 
     it 'returns the last 10 status messages' do
       expect(Status.latest.count).to eq(10)
+    end
+  end
+
+  context 'when updating only the message' do
+    before { create_list(:status, 3) }
+    let(:status) { create(:status, message: 'a message') }
+
+    it 'does not change the current status' do
+      expect{ status }.to_not change{ Status.current_status }
+    end
+
+    it 'returns the latest message' do
+      expect{ status }.to change{ Status.latest.count }.by(1)
+    end
+  end
+
+  context 'when updating only the status' do
+    before { create_list(:status, 3) }
+    let(:status) { create(:status, stat: 'down', message: nil) }
+
+    it 'changes the current status' do
+      expect{ status }.to change{ Status.current_status }.from('up').to('down')
+    end
+
+    it 'returns the latest message' do
+      expect{ status }.to_not change{ Status.latest.count }
     end
   end
 end
