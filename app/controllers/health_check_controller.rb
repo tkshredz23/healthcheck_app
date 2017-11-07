f0ee9e032a53c07597792cf6475d3530f15ed095
Tikky Shiwala <tshiwala@constantcontact.com>
2017-11-07 13:01:38 -0500

    added status and messages to homepage

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,4 +1,11 @@
 class HealthCheckController < ApplicationController
+  helper_method :current_status
+
   def index
+    @latest_messages = Status.latest
+  end
+
+  def current_status
+    Status.current_status
   end
 end
