348135b612b36c451293c0bc5445d0a337a444e9
Tikky Shiwala <tshiwala@constantcontact.com>
2017-11-07 11:26:02 -0500

    added some validation tests

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Status, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Status, type: :model do
+  it { should validate_inclusion_of(:stat).in_array(%w(up down)) }
+  it { should validate_presence_of(:stat) }
+  it { should validate_presence_of(:message) }
 end
